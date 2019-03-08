90cedb9fb36cd0dd972d70b69c42bc62e2209ee3
liuff <liu.ff_zf@sand.com.cn>
2019-03-08 15:11:00 +0800

    -a

--- webServerDemo/dynamic/mini_frameDemo.py
+++ webServerDemo/dynamic/mini_frameDemo.py
@@ -5,20 +5,7 @@
 # @DATE:   2019-03-06
 # @TIME:   13:56
 # @Software: PyCharm
-# @Production:
-
-
-def application(env, start_response):
-    start_response('200 ok', [('Content-Type', 'text/html;charset=utf-8')])
-
-    str = env['path_info']
-
-    if str == '/index':
-        return index()
-    elif str == '/login':
-        return login()
-    else:
-        return '404: hello word , 我爱中国!'
+# @Production: 实现一个简单的mini_WEN服务框架
 
 
 def index():
@@ -27,3 +14,25 @@ def index():
 
 def login():
     return '登录页'
+
+
+# 设置路由
+URL_DICT = {
+    '/index': index,
+    '/login': login,
+}
+
+
+# mini WEB 服务框架入口
+# 遵循 WSGI 协议
+def application(env, start_response):
+
+    start_response('200 ok', [('Content-Type', 'text/html;charset=utf-8')])
+    str = env['path_info']
+
+    try:
+        page = URL_DICT[str]
+        return page()
+    except Exception:
+        print('error')
+        return '404 not found,再试试别的页面吧!'
